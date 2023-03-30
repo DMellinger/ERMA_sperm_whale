e5dfeb02bb22088893a2ed754d908353cb6cf02f
David.Mellinger <David.Mellinger@oregonstate.edu>
2023-03-30 13:46:52 -0700

    new version

--- encounters.c
+++ encounters.c
@@ -1,6 +1,11 @@
 
 #include "erma.h"
 
+/* Defined below */
+static void addEncounter(int32 encStart, int32 encEnd,
+			 int32 minBlock, float blocksPerDay, ENCOUNTERS *enc);
+
+
 void initENCOUNTERS(ENCOUNTERS *enc)
 {
     enc->tSpanD = NULL;
@@ -26,6 +31,9 @@ void findEncounters(ALLCLICKS *allC, ERMAPARAMS *ep, ENCOUNTERS *enc)
     int32 secPerDay = 24*60*60;
     float blocksPerDay = (float)secPerDay / ep->blockLenS;
     float blockLenD = ep->blockLenS / secPerDay;	//block length in days
+
+    if (allC->n == 0)					//special case
+	return;
     
     /* Find first and last times of clicks */
     double minTimeD = DBL_MAX, maxTimeD = -DBL_MAX;
@@ -44,8 +52,8 @@ void findEncounters(ALLCLICKS *allC, ERMAPARAMS *ep, ENCOUNTERS *enc)
 
     for (int32 i = 0; i < nBlocks; i++) {
 	/* Find start and end of block i, in days */
-	double blockT0_D = (minBlock + i) / blocksPerDay;// block start time, days
-	double blockT1_D = blockT0_D + blockLenD;	  // block  end  time, days
+	double blockT0_D = (minBlock+i) / blocksPerDay;//block start time, days
+	double blockT1_D = blockT0_D + blockLenD;      //block  end  time, days
 	/* Count how many hits are in block i */
 	int32 nn = 0;
 	for (int32 j = 0; j < allC->n; j++)
@@ -63,8 +71,8 @@ void findEncounters(ALLCLICKS *allC, ERMAPARAMS *ep, ENCOUNTERS *enc)
     
     int32 nInConsec = 0;	//# of hits in the span (p0,p1]  (!)
     int inEnc = 0;		//currently in an encounter?
-    int32 encStart;		//start block # of encountver
-    for (int32 p0 = -ep->consecBlocks, p1 = 0; p0 < nBlocks; p0++, p1++) {
+    int32 encStartBlk;		//start block # of encounter
+    for (int32 p0 = -ep->consecBlocks, p1 = 0; p0 < nBlocks+1; p0++, p1++) {
 	nInConsec -= (p0 >= 0      ? isHit[p0] : 0);
 	nInConsec += (p1 < nBlocks ? isHit[p1] : 0);
 	if (p1 < nBlocks)
@@ -79,22 +87,23 @@ void findEncounters(ALLCLICKS *allC, ERMAPARAMS *ep, ENCOUNTERS *enc)
 	    /* Encounter continues; don't need to do anything. */
 	} else if (inEnc && ~encHere) {
 	    /* Finish encounter. It ends at last hit in the span [p0,p1). */
-	    int32 encEnd = MAX(0, p0);	//set it just in case j loop finishes
+	    int32 encEndBlk = MAX(0, p0);	//set it in case j loop finishes
 	    for (int32 j = MIN(nBlocks,p1) - 1; j >= MAX(0, p0); j--) {
 		if (isHit[j]) {
-		    encEnd = j;
+		    encEndBlk = j;
 		    break;
 		}
 	    }
-	    BUFGROW(enc->tSpanD, enc->n + 1, ERMA_NO_MEMORY_ENCOUNTERS);
-	    enc->tSpanD[enc->n][0] = (encStart + minBlock) / blocksPerDay;
-	    enc->tSpanD[enc->n][1] = (encEnd   + minBlock) / blocksPerDay;
+	    addEncounter(encStartBlk, encEndBlk, minBlock, blocksPerDay, enc);
+/*	    BUFGROW(enc->tSpanD, enc->n + 1, ERMA_NO_MEMORY_ENCOUNTERS);*/
+/*	    enc->tSpanD[enc->n][0] = (encStartBlk + minBlock) / blocksPerDay;*/
+/*	    enc->tSpanD[enc->n][1] = (encEndBlk   + minBlock) / blocksPerDay;*/
 
 	} else if (~inEnc && encHere) {
 	    /* Start an encounter at the first hit in the current p0:p1 span. */
 	    for (int32 j = MAX(0,p0); j < MIN(nBlocks,p1+1); j++) {
 		if (isHit[j]) {
-		    encStart = j;
+		    encStartBlk = j;
 		    break;
 		}
 	    }
@@ -103,7 +112,22 @@ void findEncounters(ALLCLICKS *allC, ERMAPARAMS *ep, ENCOUNTERS *enc)
 	}
 	
 	inEnc = encHere;
-    }	/* for (p0,p1...) */
+    }	//for (p0,p1...)
+    if (inEnc) {
+	//The set of files ended in the middle of an encounter. Add it.
+	addEncounter(encStartBlk, nBlocks-1, minBlock, blocksPerDay, enc);
+    }
+    printf("findEncounters: found %d encounters so far in this run\n", enc->n);
+}
+
+
+static void addEncounter(int32 encStartBlk, int32 encEndBlk,
+			 int32 minBlock, float blocksPerDay, ENCOUNTERS *enc)
+{
+    BUFGROW(enc->tSpanD, enc->n + 1, ERMA_NO_MEMORY_ENCOUNTERS);
+    enc->tSpanD[enc->n].t0 = (encStartBlk + minBlock) / (double)blocksPerDay;
+    enc->tSpanD[enc->n].t1 = (encEndBlk   + minBlock) / (double)blocksPerDay;
+    enc->n++;
 }
 
 
@@ -163,46 +187,69 @@ end
 */
 
 
-/* Save some detections -- namely, the first ep->nSaveDets (default 50) -- from
- * each encounter in goodDetsPath. Also append the name of the output file to
+/* Save some detections from each encounter in goodDetsPath. The number of
+ * detections is proportional to the duration of the encounter, relative to the
+ * total duration of all encounters.  Also append the name of the output file to
  * encFileListPath (wispr_dtx_list.txt).
  *
  * Need to also save an average spectrum.
  */
-void saveEncounters(ENCOUNTERS *enc, ALLCLICKS *allC, char *encDetsPath,
-		    double tMinE, double tMaxE, char *encFileListPath)
+void saveEncounters(ENCOUNTERS *enc, ALLCLICKS *allC, char *piEncDetsPath,
+		    char *wisprEncDetsPath, double tMinE, double tMaxE,
+		    char *encFileListPath, int32 clicksToSave)
 {
-    FILE *fp = fopen(encDetsPath, "a");
+    FILE *fp = fopen(piEncDetsPath, "a");
     const double secPerDay = 24 * 60 * 60;
 
+    /* Add up the duration of all encounters. */
+    double totalDur = 0.0;
+    for (int32 i = 0; i < enc->n; i++)
+	totalDur += TIMESPAN_DUR(enc->tSpanD[i]);
+
+/*    printf("saveEncounters: %d encounters\n", enc->n);*/
     if (fp != NULL) {
 	/* Record the start and stop of processing */
-	time_t t0 = (time_t)floor(tMinE), t1 = (time_t)floor(tMaxE);
-	fprintf(fp, "$startErma,%.3lf,%s", tMinE, asctime(gmtime(&t0)));
-	fprintf(fp,  "$stopErma,%.3lf,%s", tMaxE, asctime(gmtime(&t1)));
+/*	time_t t0 = (time_t)floor(tMinE), t1 = (time_t)floor(tMaxE);*/
+	char buf0[20], buf1[20];
+	fprintf(fp, "$analyzed,%s,%s\n",
+		(tMaxE < 0) ? "0" : timeStrE(buf0, (time_t)floor(tMinE)),
+		(tMaxE < 0) ? "0" : timeStrE(buf1, (time_t) ceil(tMaxE)));
 
 	/* Write out [some of] the clicks in this encounter. */
 	for (int32 i = 0; i < enc->n; i++) {
-	    double t0 = enc->tSpanD[i][0], t1 = enc->tSpanD[i][1];
-	    fprintf(fp, "$enc,%.3lf,%.3lf", t0 * secPerDay, t1 * secPerDay);
+	    //Figure out how many clicks to save from this encounter.
+	    double proportion = TIMESPAN_DUR(enc->tSpanD[i]) / totalDur;
+	    int32 nClicks = (int32)round(proportion * clicksToSave);
+
+	    //Print start- and stop-times of the encounter.
+	    double t0D = enc->tSpanD[i].t0, t1D = enc->tSpanD[i].t1;
+	    fprintf(fp, "$enc,%s,%s", timeStrD(buf0, t0D), timeStrD(buf1, t1D));
+
+	    //Find indices of first (i0) & last (i1) clicks in this encounter.
 	    int32 i0 = -1, i1 = -1;
 	    for (int32 j = 0; j < allC->n; j++) {
-		if (i0 == -1 && t0 < allC->timeD[j])
-		    i0 = j;
-		if (t1 < allC->timeD[j])
+		if (t0D <= allC->timeD[j] && allC->timeD[j] <= t1D) {
+		    if (i0 == -1) i0 = j;
 		    i1 = j;
+		}
 	    }
-	    for (int32 j = i0; j <= MIN(i1, i0+50); j++)
-		fprintf(fp, ",%.3lf", (allC->timeD[j] - t0) * secPerDay);
+	    //Save the clicks in the MIDDLE of the encounter, since those might
+	    //be the loudest and thus most representative of the encounter.
+	    int32 cMid = (i0 + i1)/2;
+	    int32 c0 = MAX(i0, cMid - nClicks/2);
+	    int32 c1 = MIN(i1, cMid + (nClicks+1)/2);
+	    for (int32 ci = c0; ci < c1; ci++)
+		fprintf(fp, ",%.3lf", (allC->timeD[ci] - t0D) * secPerDay);
 	    fprintf(fp, "\n");
-	}
+	} //for (i...)
 	fclose(fp);
-    }
+    } //if (fp != NULL)
 
-    /* Write name of that output file to encFileListPath */
+    /* Write name of that output file to encFileListPath. Use the WISPR version
+     * of this name. */
     fp = fopen(encFileListPath, "a");
     if (fp != NULL) {
-	fprintf(fp, "%s\n", pathFile(encDetsPath));
+	fprintf(fp, "%s\n", pathFile(wisprEncDetsPath));
 	fclose(fp);
     }
 }
